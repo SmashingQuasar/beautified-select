b1b2eb2aa75c216969754110e4468c3b264e99cf
Nicolas Cordier <n.cordier@agence-senzo.fr>
2018-05-29 17:28:37 +0200

    Release of the plugin, added Readme.md

---
+++ javascript/beautified-select.js
@@ -0,0 +1,361 @@
+"use strict";
+{
+
+    const raw_selects = document.querySelectorAll("beautified-select select"); // All the <select> tags we need to beautify.
+
+    // Creating required nodes to append.
+
+    const template_title = document.createElement("beautified-title");
+    const template_reset = document.createElement("button");
+    template_reset.type = "button";
+    template_reset.textContent = "↺";
+
+    const template_list = document.createElement("ul");
+
+    const template_select = document.createElement("beautiful-select");
+
+    const template_option = document.createElement("li");
+
+    const template_input = document.createElement("input");
+    template_input.type = "search";
+
+    const template_group = document.createElement("ul");
+
+    function beautify_option(option)
+    {
+        const beautiful_option = template_option.cloneNode();
+
+        beautiful_option.classList.add("css_option");
+        beautiful_option.dataset.index = option.index;
+        beautiful_option.textContent = option.label || option.textContent;
+
+        if (option.disabled)
+        {
+            beautiful_option.classList.add("css_disabled");
+        }
+        this.appendChild(beautiful_option);
+    }
+
+    function beautify_group(group)
+    {
+        const beautiful_group = template_group.cloneNode();
+        const options = group.querySelectorAll("option");
+        const group_title = template_option.cloneNode();
+        const group_container = template_option.cloneNode();
+
+        group_container.classList.add("css_optgroup");
+
+        if (group.disabled)
+        {
+            beautiful_group.classList.add("css_disabled");
+        }
+
+        group_title.classList.add("css_label");
+        group_title.textContent = group.label || "";
+        beautiful_group.appendChild(group_title);
+        Array.prototype.forEach.call(options, beautify_option, beautiful_group);
+        group_container.appendChild(beautiful_group);
+        this.appendChild(group_container);
+    }
+
+    function beautify_select(select)
+    {
+        let wrapper = select.closest("beautified-select");
+
+        // Handling incorrect select configuration.
+
+        if (!wrapper)
+        {
+            throw new Error("Couldn't beautify select since it is not child of a <beautified-select> tag.");
+            return false;
+        }
+
+        select.hidden = true;
+
+        if (select.multiple)
+        {
+            wrapper.classList.add("css_multiple");
+        }
+        document.querySelector("form").reset();
+        const default_selection = select.selectedIndex;
+
+        const beautiful_title = template_title.cloneNode();
+
+        const beautiful_reset = template_reset.cloneNode(true);
+        if (select.dataset.reset === undefined)
+        {
+            beautiful_reset.hidden = true;
+        }
+        const beautiful_list = template_list.cloneNode();
+
+        const default_option = select.options[default_selection];
+
+        beautiful_title.textContent = default_option && default_option.textContent ? default_option.textContent : select.dataset.placeholder;
+
+        const raw_groups = select.querySelectorAll("optgroup");
+
+        if (raw_groups)
+        {
+            Array.prototype.forEach.call(raw_groups, beautify_group, beautiful_list);
+            const stray_options = wrapper.querySelectorAll("select > option");
+
+            if (stray_options)
+            {
+                Array.prototype.forEach.call(stray_options, beautify_option, beautiful_list);
+            }
+
+        }
+        else
+        {
+            const raw_options = select.querySelectorAll("option");
+            Array.prototype.forEach.call(raw_options, beautify_option, beautiful_list);
+        }
+        
+        let container = template_select.cloneNode();
+        select.insertAdjacentElement("afterend", container);
+
+        container.appendChild(beautiful_title);
+        container.appendChild(beautiful_reset);
+        container.appendChild(beautiful_list);
+        
+        // Taking CSS affected borders in account for smooth display.
+
+        let computed_style = window.getComputedStyle(beautiful_list);
+        let bottom_border = +computed_style.borderBottomWidth.match(/[0-9]+/);
+        let top_border = +computed_style.borderTopWidth.match(/[0-9]+/);
+        let total_border = bottom_border + top_border;
+        let children = beautiful_list.querySelectorAll("li.css_option");
+        let base_offsetHeight = beautiful_list.querySelector("li.css_option").offsetHeight;
+
+        if (select.size)
+        {
+            if (select.size < children.length)
+            {
+                beautiful_list.dataset.height = (base_offsetHeight * select.size) + total_border;
+            }
+            else
+            {
+                beautiful_list.dataset.height = (base_offsetHeight * children.length) + total_border;
+            }
+        }
+        else
+        {
+            if (3 < children.length)
+            {
+                beautiful_list.dataset.height = (base_offsetHeight * 3) + total_border;
+            }
+            else
+            {
+                beautiful_list.dataset.height = (base_offsetHeight * children.length) + total_border;
+            }
+        }
+
+        beautiful_list.style.height = 0;
+
+        if (select.dataset.autocomplete === "true")
+        {
+            const beautiful_input = template_input.cloneNode();
+
+            container.appendChild(beautiful_input);
+
+            beautiful_input.addEventListener(
+                "input",
+                function (event)
+                {
+                    if (wrapper.classList.contains("css_active"))
+                    {
+                        Array.prototype.forEach.call(
+                            beautiful_list.querySelectorAll("li.css_option"),
+                            function (option)
+                            {
+                                option.hidden = event.target.value && !option.textContent.toLowerCase().match(event.target.value.toLowerCase());
+                            }
+                        );
+                    }
+
+                }
+            );
+
+        }
+
+        wrapper.addEventListener(
+            "click",
+            function (event)
+            {
+                const target = event.target.closest("beautified-title, li, button");
+
+                if (!target)
+                {
+                    return;
+                }
+                switch (target.nodeName)
+                {
+                    case "BEAUTIFIED-TITLE":
+
+                        if (!select.disabled)
+                        {
+                            wrapper.classList.toggle("css_active");
+                            beautiful_list.style.height = (beautiful_list.offsetHeight - total_border) ? 0 : beautiful_list.dataset.height + "px";
+                            if (select.dataset.autocomplete === "true")
+                            {
+                                wrapper.querySelector("input[type=search]").focus();
+                            }
+                        }
+
+                        break;
+
+                    case "LI":
+
+                        if (event.target.classList.contains("css_option") && !event.target.closest(".css_disabled"))
+                        {
+
+                            if (!select.multiple)
+                            {
+                                Array.prototype.forEach.call(
+                                    beautiful_list.childNodes,
+                                    function (list_item)
+                                    {
+                                        list_item.classList.remove("css_selected");
+                                    }
+                                );
+                            }
+
+                            target.classList.toggle("css_selected");
+                            select.options[+target.dataset.index].selected = !select.options[+target.dataset.index].selected;
+
+                            if (!select.multiple)
+                            {
+                                beautiful_title.textContent = event.target.textContent || select.dataset.placeholder;
+                                wrapper.classList.remove("css_active");
+                                beautiful_list.style.height = 0;
+                                if (select.dataset.autocomplete === "true")
+                                {
+                                    wrapper.querySelector("input[type=search]").value = null;
+                                    Array.prototype.forEach.call(
+                                        beautiful_list.querySelectorAll("li.css_option"),
+                                        function (option)
+                                        {
+                                            option.hidden = false;
+                                        }
+                                    );
+                                }
+                            }
+                            else
+                            {
+                                const checked_options = select.querySelectorAll("option:checked");
+
+                                let regexp = new RegExp(",? ?(" + target.textContent + ",?)", "g");
+                                let match = beautiful_title.textContent.match(regexp);
+
+
+                                if (match)
+                                {
+                                    beautiful_title.textContent = beautiful_title.textContent.replace(match[0], "");
+                                    if (!checked_options.length)
+                                    {
+                                        beautiful_title.textContent = select.dataset.placeholder || "";
+                                    }
+                                }
+                                else
+                                {
+                                    if (checked_options.length === 1)
+                                    {
+                                        beautiful_title.textContent = target.textContent;
+                                    }
+                                    else
+                                    {
+                                        beautiful_title.textContent += ", " + target.textContent;
+                                    }
+                                }
+                            }
+                            select.dispatchEvent(new CustomEvent("change"));
+
+                        }
+                        break;
+
+                    case "BUTTON":
+                        
+                        if (!select.disabled)
+                        {
+                            Array.prototype.forEach.call(
+                                wrapper.querySelectorAll("li.css_option"),
+                                function (list_item)
+                                {
+                                    list_item.classList.remove("css_selected");
+                                }
+                            );
+                            Array.prototype.forEach.call(
+                                select.options,
+                                function (option)
+                                {
+                                    option.selected = false;
+                                }
+                            );
+                            if (select.multiple)
+                            {
+                                beautiful_title.textContent = select.dataset.placeholder || "";
+                            }
+                            else
+                            {
+                                beautiful_list.childNodes[default_selection].classList.add("css_selected");
+                                beautiful_title.textContent = default_option.textContent || select.dataset.placeholder;
+                                default_option.selected = true;
+                            }
+                            if (select.dataset.autocomplete === "true")
+                            {
+                                wrapper.querySelector("input[type=search]").value = null;
+                                Array.prototype.forEach.call(
+                                    beautiful_list.querySelectorAll("li.css_option"),
+                                    function (option)
+                                    {
+                                        option.hidden = false;
+                                    }
+                                );
+                            }
+                            select.dispatchEvent(new CustomEvent("change"));
+                        }
+                        break;
+                }
+            }
+        );
+    }
+
+    Array.prototype.forEach.call(raw_selects, beautify_select);
+    
+    document.addEventListener(
+        "click",
+        function (event)
+        {
+            Array.prototype.forEach.call(
+                document.querySelectorAll("beautified-select"),
+                function (beautified_select)
+                {
+                    if (beautified_select !== event.target.closest("beautified-select"))
+                    {
+                        beautified_select.classList.remove("css_active");
+                        let list = beautified_select.querySelector("ul");
+                        let style = window.getComputedStyle(list);
+                        let borders = style.borderBottomWidth.match(/[0-9]+/) + style.borderTopWidth.match(/[0-9]+/);
+                        list.style.height = (list.offsetHeight - borders) ? 0 : list.dataset.height + "px";
+
+                        if (beautified_select.querySelector("select").dataset.autocomplete === "true")
+                        {
+                            beautified_select.querySelector("input[type=search]").value = null;
+
+                            Array.prototype.forEach.call(
+                                beautified_select.querySelectorAll("li.css_option"),
+                                function (option)
+                                {
+                                    option.hidden = false;
+                                }
+                            );
+                        }
+                    }
+                }
+            );
+        }
+    );
+    
+    window.beautify_select = beautify_select;
+
+}
---
+++ readme.md
@@ -0,0 +1,194 @@
+# Beautified Select
+
+This plugin aims to ease the beautifying process of HTML 5 `<select>` tags. You do not require any JavaScript knowledge to use this plugin as it is based on the HTML standard and uses the default HTML syntax. Please read the following sections to have more information on the plugin syntax.
+
+## Licence
+
+> Copyright (c) 2018 Nicolas "SmashingQuasar" Cordier
+> 
+> Permission is hereby granted, free of charge, to any person obtaining a copy
+> of this software and associated documentation files (the "Software"), to deal
+> in the Software without restriction, including without limitation the rights
+> to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+> copies of the Software, and to permit persons to whom the Software is
+> furnished to do so, subject to the following conditions:
+> 
+> The above copyright notice and this permission notice shall be included in all
+> copies or substantial portions of the Software.
+> 
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+> IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+> FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+> AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+> LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+> OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+> SOFTWARE.
+
+## Reviews of files
+
+### beautified_select.html
+
+This is a sample file provided just to give you 5 examples of how to use this plugin.
+
+### reset.css
+
+This file is a basic CSS reset file, it is provided to make the default CSS work in any case. If you intend to change the style of the plugin or if you already have your reset.css file, you do not need it.
+
+### style.css
+
+This file contains only the default style for this plugin. It uses low weight CSS selectors so you can easily override them.
+It is not required BUT the plugin will NOT work without a minimum style added to it!
+
+### beautified-select.js
+
+The main JavaScript file for this plugin. This file is required for the plugin to work. It does not provide any polyfill or load any external library and is fully standalone.
+
+## A quick word about CSS
+
+This plugin has been written with a default style so it can work properly when loaded. The default CSS provided with this plugin has the lowest possible weight, which means you can easily override it with your own CSS at your convenience.
+
+## Requirements
+
+This plugin requires the following JavaScript functionalities to work properly:
+
+* Element.closest() function: For Internet Explorer 11 you will need to use a polyfill.
+* CustomEvent class: For Internet Explorer 11 you will need a polyfill for this class to work.
+
+## Getting Started
+
+To use the plugin, load the _style.css_ file and the _beautified-select.js_ script in your webpage.
+
+Since this is a non-intrusive plugin trying not to mess with other scripts/plugins, you will need to surround the `<select>` tags you want to beautify with a custom tag : `<beautified-select>`.
+Once you have surrounded your `<select>` with a `<beautified-select>` custom tag, the plugin should beautify it accordingly.
+
+## Basic HTML5 features
+
+The plugin will adapt the display of your beautified select to the attributes of your select tag and its inner HTML.
+
+### Simple & Multiple `<select>`
+
+Just as you would do with a basic HTML5 select, you can create a multiple select with this plugin. Simply add the attribute `multiple` to your `<select>` tag (not the `<beautified-select>` tag!) and refresh your webpage.
+
+Example:
+```html
+<!-- This configuration will allow the user to select any of the option or both -->
+
+<beautified-select>
+    <select multiple name="sample">
+        <option value="foo">Foo</option>
+        <option value="bar">Bar</option>
+    </select>
+</beautified-select>
+```
+
+### Managing number of displayed items
+
+Beautified Select fully supports the `size` attribute of the `<select>` tag. By default most browser display 3 elements inside `<select>` dropdowns, so this is the default value for Beautified Select as well. If you want to change it, simply add the `size="x"` attribute to your `<select>` tag.
+
+Example:
+```html
+<!-- This configuration will display "Foo", "Bar", "Smashing", "Quasar" in the dropdown -->
+
+<beautified-select>
+    <select name="sample" size="4">
+        <option value="foo">Foo</option>
+        <option value="bar">Bar</option>
+        <option value="smashing">Smashing</option>
+        <option value="quasar">Quasar</option>
+        <option value="zamralik">Zamralik</option>
+    </select>
+</beautified-select>
+```
+
+### Optgroups
+
+You can define HTML5 `<optgroup>` tags within your `<select>` innerHTML and Beautified Select will parse them and display an embellished version of them.
+The generated DOM will work exactly as a vanilla `<optgroup>` would and will not be selectable.
+Please refer to the official HTML standard if you want to know more about `<optgroup>`: [WhatWG](https://html.spec.whatwg.org/multipage/form-elements.html#the-optgroup-element)
+
+Example:
+```html
+<!-- This configuration will display two <optgroup>: "Zamralik" and "Default" -->
+
+<beautified-select>
+    <select name="sample">
+        <optgroup label="Zamralik">
+            <option value="foo">Foo</option>
+            <option value="bar">Bar</option>
+        </optgroup>
+        <optgroup label="Default">
+            <option value="smashing">Smashing</option>
+            <option value="quasar">Quasar</option>
+        </optgroup>
+        <option value="zamralik">Zamralik</option>
+    </select>
+</beautified-select>
+```
+
+## Advanced features
+
+Beautified Select goes further in the customization and allows you to add advanced features simply by adding a data-attribute in your select.
+
+### Autocompletion
+
+You can enable an autocompletion feature to help users find what they are looking for in vastly populated lists.
+To add this feature, simply add the following attribute to your `<select>` tag: `data-autocomplete="true"`
+An important note is to understand this autocomplete feature only search within `<option>` tags and not `<optgroup>` as its primary goal is to provide easily selectable options to the user.
+
+Example:
+```html
+<!-- This configuration will enable the autocompletion for this select -->
+
+<beautified-select>
+    <select name="sample" data-autocomplete="true">
+        <option value="foo">Foo</option>
+        <option value="bar">Bar</option>
+        <option value="smashing">Smashing</option>
+        <option value="quasar">Quasar</option>
+        <option value="zamralik">Zamralik</option>
+    </select>
+</beautified-select>
+```
+
+### Placeholder
+
+The HTML standard does not provide a way to define a placeholder value for `<select>` tag. Beautified Select offers this possibility.
+To defined a placeholder, you just need to add a data-attribute as follows: `data-placeholder="Choisissez une valeur"` in your `<select>` tag definition.
+
+Example:
+```html
+<!-- This configuration will enable the autocompletion for this select -->
+
+<beautified-select>
+    <select name="sample" data-placeholder="Please select a value">
+        <option value="foo">Foo</option>
+        <option value="bar">Bar</option>
+        <option value="smashing">Smashing</option>
+        <option value="quasar">Quasar</option>
+        <option value="zamralik">Zamralik</option>
+    </select>
+</beautified-select>
+```
+
+> Note: In vanilla HMTL you would simply define an option with an empty value to define a placeholder. This is fully supported by Beautified Select.
+> If you have defined an empty option with an empty value AND a data-placeholder, then selecting the empty option will display the placeholder.
+
+### Reset value
+
+Beautified Select provides a simple way to reset the value within the select.
+To enable the reset button, please add the following data-attribute to your `<select>` tag: `data-reset`:
+
+Example:
+```html
+<!-- This configuration will enable the autocompletion for this select -->
+
+<beautified-select>
+    <select name="sample" data-reset>
+        <option value="foo">Foo</option>
+        <option value="bar">Bar</option>
+        <option value="smashing">Smashing</option>
+        <option value="quasar">Quasar</option>
+        <option value="zamralik">Zamralik</option>
+    </select>
+</beautified-select>
+```
