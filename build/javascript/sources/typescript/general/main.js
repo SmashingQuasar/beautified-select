feb9081264d4b1c87137815d3cb3b9be82375599
Nicolas Cordier <nicolas.cordier@outlook.com>
2020-12-31 12:14:50 +0100

    Added a small fix to permit placeholder usage

--- build/javascript/sources/typescript/general/main.js
+++ build/javascript/sources/typescript/general/main.js
@@ -1,6 +1,6 @@
 import { BeautifiedSelect } from "../Model/BeautifiedSelect.js";
 const SELECTS = document.querySelectorAll("select");
 Array.from(SELECTS).forEach((select) => {
-    new BeautifiedSelect(select, { placeholder: "Select a value..." });
+    new BeautifiedSelect(select, { placeholder: select.dataset.placeholder === undefined ? "Sélectionner une valeur" : select.dataset.placeholder });
 });
 //# sourceMappingURL=main.js.map
--- sources/typescript/general/main.ts
+++ sources/typescript/general/main.ts
@@ -4,6 +4,6 @@ const SELECTS: NodeListOf<HTMLSelectElement> = document.querySelectorAll("select
 
 Array.from(SELECTS).forEach(
     (select: HTMLSelectElement): void => {
-        new BeautifiedSelect(select, { placeholder: "Select a value..." });
+        new BeautifiedSelect(select, { placeholder: select.dataset.placeholder === undefined ? "Sélectionner une valeur" : select.dataset.placeholder });
     }
 );
